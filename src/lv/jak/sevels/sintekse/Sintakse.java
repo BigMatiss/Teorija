3e59abae35f8b7f44064566098e01ead6eb12218
lietotajs <lietotajs@121-16>
2019-12-20 10:56:55 +0200

    Aprakstīta kalse SINTAKE

---
+++ src/lv/jak/sevels/sintekse/Sintakse.java
@@ -0,0 +1,25 @@
+// Komentārs - programmas koda dala, kas palīdz programmas izstrādātājiem labāk 
+// izprast koda darbibas principus. Programmas darbības laikā komentāri netiek ņemti vērā 
+
+// - Viens rindas komentārs 
+
+/* 
+ * Vairāku
+ * rindu 
+ * komentārs
+ */
+
+package lv.jak.sevels.sintekse;  //pakotne - klases atrašanās vieta projektā
+
+//import komanda - komandrinda,kas nodrošina citas klases vai pakotnes pieskaitīti šai klasei.
+//Tāā veidā tiek nodrošina dažādu funkciju izmantošanas iespējas šajā klasē, piemeram,
+//datu ievade no tastatūras 
+
+public class Sintakse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
