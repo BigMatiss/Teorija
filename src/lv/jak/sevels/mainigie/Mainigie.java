accef02a1e03b6d029810b1e3a96151c06bfe18c
lietotajs <lietotajs@121-16>
2020-01-17 11:41:00 +0200

    Projekta sakārtošana (A.Ašaks)

---
+++ src/lv/jak/sevels/mainigie/Mainigie.java
@@ -0,0 +1,31 @@
+package lv.jak.sevels.mainigie;
+
+public class Mainigie {
+
+	public static void main(String[] args) {
+		//Mainīgā defenicija - norada , ka izmantos ,ainigais 
+		// vispariga forma: <datuTips> <mainigaNoasaukums>
+		int veselsSkaitlis; 
+		double decimalsSkaitlis;
+		
+		// ja tiek paradzets vienadam datu tipam izmantot vairakus mainigos ar vienādu datu tipu,
+		//tos var uzskatit viena rinda atdalot a komatiem 
+		//vispariga forma: <datuTips> <mainigaNoasaukums>, <mainiganosaukums2>;
+		int saitlis1, skaitlis2;
+		
+		// mainiga initalizacija - mianiga pirmerizeja azpildisana ar vertibu 
+		//--- ja ieprieksejie ir defnets mainigais 
+		//vispariga forma: <datuTips> <mainigaNoasaukums> = <vertibe>;
+		veselsSkaitlis = 5;
+		decimalsSkaitlis = 9.5;
+		
+		// ---ja izprieks nav defnets mainigais 
+		// vispariga forma: <datuTips> <mainigaNosaukums> = <vertība>;
+		int jaunsSkaitlis = 56;
+		
+		int jaunsSkaitlis1, jaunsSkaitlis2 = 45, jaunsskaitlis3 = veselsSkaitlis;
+				
+	}
+
+
+}
--- src/lv/jak/sevels/sintekse/Sintakse.java
+++ src/lv/jak/sevels/sintekse/Sintakse.java
@@ -12,14 +12,68 @@
 package lv.jak.sevels.sintekse;  //pakotne - klases atrašanās vieta projektā
 
 //import komanda - komandrinda,kas nodrošina citas klases vai pakotnes pieskaitīti šai klasei.
-//Tāā veidā tiek nodrošina dažādu funkciju izmantošanas iespējas šajā klasē, piemeram,
+//Tā veidā tiek nodrošina dažādu funkciju izmantošanas iespējas šajā klasē, piemeram,
 //datu ievade no tastatūras 
+/*
+ * iespējamas pieraksta formas 
+ * import <pakotne>.<klase>;
+ * import <pakotne>.*;
+ */
+import java.util.Scanner;
 
+// JAVA klases daļa - galvenā programmēšanas daļa
+// Sinktakse - Klaes nosaukums 
+/*
+ * Sintakses nosacījumi uz klases nosaukumu 
+ *      1. Kalses nosaukums sakas ar lielo burtu
+ *      2. Ja klases nosaukums satur vairākus vārdus, katrs vārds sakas ar lielo burtu 
+ *      3. Nosaukumas var saturēt TIKIA BURTUS un CIPARUS!!!
+ *      4. Ja klases nosaukums satur vairākus vārdus, tas ar atstarpēm NEATDLA!!!
+ */
 public class Sintakse {
-
+	
+	
+	// Klases kermenis - šajā daļā aprakstītais koda faragments jr pieejams visai klasei,
+	// izņēmuma nosacjyms  - apraksītās lietas ir radzamas tikai tai daļai koda , kas atrodas zemāk.
+	// !!! NEDRĪKST rakstīt funkcionālus procesus, izņemot vēŗtības vērtibas vai objekta izveidi
+	
+	//Mainīgie - programmas valodas pamatelements
+	/*
+	 * Sintakses nosacijumi un mainīgā nosaukumu 
+	 *      1.Mainīgā nosaukums sākas ar mazo kurtu
+	 *      2.Ja mainīgā nosaukums satur vairākus vārdus,
+	 *      pirmais vārds ar mazo,kats nākamais vārds sākas ar lielo burtu 
+	 *      3.Ja mainīgā nosaukums satur vairākus vārdus, tas ar atstarpēm NEATDLA!!!
+	 *      4.Nosaukumas var saturēt TIKIA BURTUS un CIPARUS!!!
+	 * Izņēmuma gadijumi:  
+	 *      1.Ja maiīgais uzglabā NEMAINĪGI (KONSTANTU) vērtību, nosaukums jāraksta ar lieliem burtiem un apakšvītrām (ja nosaukumā ir vairāki vārdi)
+	 *      2.    
+	 */
+	// Mainīgā izvedošana
+	/* Pierakasta forma:
+	 *       --> ja ieprieķšējā mainīgais NAV izveidots: <datuTips> <mainīgaNosaukums>;      
+	 */
+	static int veselsSkaitlis;
+	
+	// vērtības piešķiršanas mainīgajam 
+	/* Pieraksta forma:
+	 *     --> ja ieprieķšējā mainīgais NAV izveidots: <datuTips> <mainīgaNosaukums> = <vērtiba>;
+	 *     --> ja ieprieķšējā mainīgais IR  izveidots: <mainīgaNosaukums> = <vērtības>;
+	 *     (Šo procesu var veikt TIKAI metodēs)
+	 */
+	double PI_VERTIBA = 4.14;  // konstants mainīgais
+	
+    // metode MAIN - galvenā moetodi.
+	/*
+	 * MAIN metode:
+	 *      1. Uz visu projektu jābūt VISMAZ 1 MAIN metodei 
+	 *      2. Metodes pieaksts ir TIEŠI 
+	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        //metodes kermenis - aprakstam algoritmu, kuru vēlamies izpildt
+		veselsSkaitlis = 5; //vērtības piešķiršana jau eksistējošam mainīgajam 
 	}
-
+// Klases kermenis ir beizdies 
 }
+// NEKO ŠEIT NEDĪKST RAKSTĪT.
